9f20e4038a9aa65ca500e591eaf6b9a7dc4a43b3
Andriy-sys <marchycandriy@gmail.com>
2022-01-20 20:26:28 +0200

    HTML and Js

---
+++ Task2/app.js
@@ -0,0 +1,34 @@
+class News {
+  _head;
+  _text;
+  _tegs;
+  _date;
+  _now = new Date();
+  constructor(head = '', text = '', tegs = [], date) {
+    this._head = head;
+    this._text = text;
+    this._tegs = [...tegs];
+    this._date = date;
+  }
+
+  print() {
+    document.write(`<h1>${this._head}</h1>`);
+    if (this._now.getDay() == this._date) {
+      document.writeln(`сегодня`);
+    } else if (this._date > 1 && this._date < 7) {
+      document.writeln(
+        `${parseInt(this._now.getDay()) - parseInt(this._date)} дней назад`
+      );
+    } else {
+      document.writeln(`${this._date}`);
+    }
+    document.writeln(`<p>${this._text}</p>`);
+    for (let i = 0; i < this._tegs.length; i++) {
+      document.write(`${this._tegs[i]}`);
+    }
+  }
+}
+
+let teg = ['#app', '#prog', '#web'];
+let _new = new News('Andri', 'How are you?', teg, '20-01-2022');
+_new.print();
